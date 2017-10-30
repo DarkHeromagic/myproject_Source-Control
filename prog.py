3353c78c6e7b95815f230c118ed12a2742f81c77
Zeng Dong <Charleszengdong@gmail.com>
2017-10-30 09:05:35 +0800

    First commit

--- prog.py
+++ prog.py
@@ -1,1 +1,20 @@
-import sys
+#!/user/bin/python
+
+def main():
+    
+    print "Guess a number bewteen 1 and 100."
+    randomNumber = 50
+    found = False
+    while not found:
+        userGuess = input("Your guess: ")
+        if userGuess == randomNumber:
+            print "You got it!"
+            found = True
+        elif userGuess > randomNumber:
+            print "Too big"
+        else:
+            print "Too small"
+
+if _name_ == "_main_":
+    main()
+
